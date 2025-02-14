5ffb0cb295f1c816f8f9bf31437cf10406dcb62b
Ethan Bernhardt <122914150+Ethansb16@users.noreply.github.com>
2025-02-13 19:07:43 -0800

    Alpha generator attack for DH key exchange

---
+++ alpha-Generator.py
@@ -0,0 +1,70 @@
+from Crypto.Random import random, get_random_bytes
+from hashlib import sha256
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+
+
+iv = get_random_bytes(AES.block_size)
+
+def power(a, b, p): 
+    return pow(a, b, p)
+
+def generate_private_key(a): 
+    return random.randint(1, a-1)
+
+def generate_public_key(private_key, q, a): 
+    return pow(a, private_key) % q
+
+def compute_shared_secret(their_public_key, private_key, q):
+    return pow(their_public_key, private_key) % q
+
+def send_message(original_message, iv, sender_key,  reciever_key):
+    """Simulates sending a message between users with """
+    message = pad(original_message, AES.block_size)
+    cipher = AES.new(sender_key, AES.MODE_CBC, iv=iv)
+    encryption = cipher.encrypt(message)
+    print(f"encrypted message: {original_message} -> {encryption}")
+    cipher = AES.new(reciever_key, AES.MODE_CBC, iv= iv)
+    return unpad(cipher.decrypt(encryption), AES.block_size)
+
+def main(): 
+    alpha = 1
+
+    q = int("B10B8F96A080E01DDE92DE5EAE5D54EC52C99FBCFB06A3C69A6A9DCA52D23B616073E28675A23D189838EF1E2EE652C013ECB4AEA906112324975C3CD49B83BFACCBDD7D90C4BD7098488E9C219A73724EFFD6FAE5644738FAA31A4FF55BCCC0A151AF5F0DC8B4BD45BF37DF365C1A65E68CFDA76D4DA708DF1FB2BC2E4A4371", 16)
+    
+    print(f"value of q: {q}")
+    print(f"value of alpha: {alpha}")
+
+    #alice keys
+    XA = generate_private_key(q)
+    YA = power(alpha, XA, q)
+
+    print(f"alice private key: {XA}")
+    print(f"alice public key: {YA}")
+
+    #bob keys
+    XB = generate_private_key(q)
+    YB = power(alpha, XB, q)
+
+    print(f"bob public key: {XB}")
+    print(f"bob private key: {YB}")
+
+    #shared key
+    k_alice = power(YB, XA, q)
+    k_bob = power(YA, XB, q)
+    print(f"alice shared key: {k_alice}")
+    print(f"bob shared key: {k_bob}")
+
+    sha_alice = sha256(str(k_alice).encode()).digest()[:16]
+    sha_bob = sha256(str(k_bob).encode()).digest()[:16]
+    
+    m0 = b'Hi Alice!'
+    m1 = b'Hi Bob!'
+    
+    print("Intercepted messages")
+    print(send_message(m0, iv, sha_bob, sha_alice))
+    print(send_message(m1, iv, sha_bob, sha_alice))
+
+
+if __name__ == "__main__": 
+    main()
